376f7439eb864e8090ffa56f89ff00f24e0aa8e0
samick17 <boneache@gmail.com>
2018-06-02 00:28:44 +0800

    1. update version to 1.0.14 2. replace module htmlparser2 with node-xml-parser 3. update abi version data

---
+++ abi-map-html-parser.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const xmlParser = require('node-xml-parser');
+
+function createABIJson(nodeAbi, callback) {
+  var filePath = path.resolve('./nodeabi.json');
+  var data = JSON.stringify(nodeAbi);
+  fs.writeFile(filePath, data, function(err) {
+    if(err) {
+      callback(err);
+    } else {
+      callback(null, nodeAbi);
+    }
+  });
+}
+
+module.exports = {
+  parse: function(callback) {
+    var nodeAbiMap = {};
+    xmlParser.parseFromFile(path.resolve('./versions-list'))
+    .then((nodes) => {
+      nodes[0].forEach(function(trNode) {
+        var key, value;
+        trNode.forEach(function(tdNode, index) {
+          if(index === 0) {
+            key = tdNode.value.replace(/[a-zA-Z\.]*\s/g, '');
+          } else if(index === 5) {
+            value = tdNode.value;
+            nodeAbiMap[key] = value;
+            key = '';
+            value = '';
+          }
+        });
+      });
+      createABIJson(nodeAbiMap, callback);
+    }, (err) => {
+      callback(err);
+    });
+  }
+};
+
+if(module.id === '.') {
+  var api = module.exports;
+  api.parse(function(err, abiVersionsMap) {
+    console.log(err, abiVersionsMap);
+  });
+}
